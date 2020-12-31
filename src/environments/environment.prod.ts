a2dbf3cdd397b7af978e5a67ad8910324c61c6c4
Rijin <rijink.121@gmail.com>
2020-12-31 07:22:54 +0530

    prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   APP_NAME: 'Wedding-App',
-  API_ENDPOINT: 'http://localhost:3000',
+  API_ENDPOINT: 'http://ec2-13-232-40-253.ap-south-1.compute.amazonaws.com:3000',
   DATE_FORMAT: 'MM/dd/yyyy',
   DATE_TIME_FORMAT: 'MM/dd/yyyy hh:mm a',
   HASH_KEY: 'Wedding-App-Hash',
